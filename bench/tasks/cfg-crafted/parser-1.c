b4bc344b63c769a184695ea5efb45803ea6cb16e
Nikhil Pimpalkhare <nikhil.pimpalkhare@princeton.edu>
2023-04-24 18:29:35 -0400

    parser update

--- bench/tasks/cfg-crafted/parser-1.c
+++ bench/tasks/cfg-crafted/parser-1.c
@@ -1,41 +1,29 @@
+int end;
 int start;
-int end; 
-char EOF = -1;
+char EOF;
 
-char lexer() {
-    char c = __VERIFIER_nondet_char();
-    if (c == '\0') {
-        end += 1;
-        start = end;
-    } else {
-        end += 1;
-    }
-    return c;
+void container1() {
+    end += 1;
+    start = end;
 }
-
-void parser() {
-    char token[10];
-    int i = 0;
-    char next = lexer();
-    while (next != '\0') {
-        token[i] = next;
-        i++;
-        next = lexer();
-    }
-
-    // do something with token
-
-    if (*token == EOF) {
-        return;
+void container2() {
+    end += 1;
+}
+char lexer(char* s, int slen) {
+    if (slen <= 0) {return EOF;}
+    char c = s[0];
+    if (c == '\0') {
+        container1();
     } else {
-        parser();
+        container2();
     }
+    lexer(s + 1, slen - 1);
 }
 
 int main() {
-    start = __VERIFIER_nondet_int();
-    end = start;
-    parser();
-    __VERIFIER_assert(start <= end);
-    return 0;
+        start = __VERIFIER_nondet_int();
+        end = start;
+        lexer(0, __VERIFIER_nondet_int());
+        __VERIFIER_assert(start <= end);
+        return 0;
 }
