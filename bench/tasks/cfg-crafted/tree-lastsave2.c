5643e283fc32b77ae385f97830f2ea12a32e45dc
Nikhil Pimpalkhare <nikhil.pimpalkhare@princeton.edu>
2023-07-09 16:33:15 -0700

    fixed some tasks

--- bench/tasks/cfg-crafted/tree-lastsave2.c
+++ bench/tasks/cfg-crafted/tree-lastsave2.c
@@ -9,7 +9,7 @@ int last_save;
 void tree() {
     node_number += 1;
     if (__VERIFIER_nondet_int()) {
-        mem_ops += 3 * (node_number - last_save);
+        mem_ops += (node_number - last_save);
         last_save = node_number;
         return;
     } else {
@@ -24,7 +24,7 @@ void tree() {
 int main() {
     node_number = 0; mem_ops = 0; last_save = 0;
     tree();
-    if (!(mem_ops <= 4 * node_number)) {
+    if (!(mem_ops <= 2 * node_number)) {
             ERROR: {reach_error();abort();}
         }
     return 0;
